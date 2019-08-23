0dc847101db207bf65e7611c7505cd413f13a890
asathiya007 <akshay.sathiya@gmail.com>
2019-08-22 20:06:17 -0400

    started carousel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carousel from "react-bootstrap/Carousel";
+import HomePage from "./components/HomePage/HomePage";
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel className="carousel" interval={null}>
+        <Carousel.Item>
+          <HomePage />
+        </Carousel.Item>
+        <Carousel.Item>
+          <HomePage />        
+        </Carousel.Item>
+        <Carousel.Item>
+          <HomePage />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
