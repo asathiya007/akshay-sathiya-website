682ea284cdff25cc49a19ae513786662d4c25481
asathiya007 <akshay.sathiya@gmail.com>
2019-08-24 23:27:16 -0400

    prepared for deployment

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./HomePage.css";
 
 const HomePage = () => {
+    const nameClicked = () => {
+        window.open("https://akshaysathiya.herokuapp.com/");
+    }
+
     return (
         <div className="content center pt4">
             <div className="title">
-                <h1 className="grow">AKSHAY SATHIYA</h1>
+                <h1 className="grow" onClick={nameClicked}>AKSHAY SATHIYA</h1>
                 <div className="typewriter grow">
                     <h3 className="bg-gray br3 ph2" style={{fontSize: "2vw"}}>Hey there, welcome to my website!</h3>
                 </div>
