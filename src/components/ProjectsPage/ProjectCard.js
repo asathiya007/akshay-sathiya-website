6c1013c7db116e9b5ada8d96a6c81e3e18cbc531
asathiya007 <akshay.sathiya@gmail.com>
2019-08-24 21:45:59 -0400

    did some more modifications to projects page and tested mods.

--- src/components/ProjectsPage/ProjectCard.js
+++ src/components/ProjectsPage/ProjectCard.js
@@ -9,23 +9,25 @@ const ProjectCard = ({ image, name, descriptions, link, linkType }) => {
 
     return (
         <article className="br3 ba dark-gray bg-near-white b--black-10 w-30 ma3">
-            <div className="w-100 bg-black br3 br--top center pa2">
-                <img src={image} className="db br3" alt="" height="200px"/>
+            <div className="w-100 bg-black br3 br--top center pa2 vh-25">
+                <img src={image} className="db br3" alt="" height="95%"/>
             </div>
-            <div className="pa2 ph3-ns pb3-ns" style={{height: "38vh"}}>
-                <div className="dt w-100 mt1">
-                    <div className="dtc">
-                        <h1 className="mv0" style={{ fontSize: "1.5vw" }}>{name}</h1>
-                    </div>
+            <div className="dt w-100 mt1 pv2">
+                <div className="dtc">
+                    <h1 className="mv0" style={{ fontSize: "1.5vw" }}>{name}</h1>
                 </div>
-                <div className="f6 lh-copy mt2 mid-gray w-100" style={{ fontSize: "0.8vw"}}>
+            </div>
+            <div className="pa2 ph3-ns pb3-ns center" style={{height: "32vh"}}>
+                <div className="f6 lh-copy mt2 mid-gray w-100" style={{ fontSize: "0.9vw"}}>
                     {
                         descriptions.map((desc, i) => (
                             <p key={i}>{desc}</p>
                         ))
                     }
                 </div>
-                <Button variant="primary" onClick={cardClicked} style={{ fontSize: "0.8vw"}}>{linkType}</Button>
+            </div>
+            <div className="ma3">
+                <Button variant="primary" onClick={cardClicked} style={{ fontSize: "1vw" }} >See {linkType}</Button>
             </div>
         </article>
     );
--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: "scroll", height: "78vh"}} className="mt4 mb3">
+        <div style={{overflowY: "scroll", height: "83vh"}} className="mt4 mb3">
             {props.children}
         </div>
     );
