7dcc19e17856ef934384c4631eb2d4b9c832793d
asathiya007 <akshay.sathiya@gmail.com>
2019-08-24 22:39:40 -0400

    verified all functionality

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,9 +3,12 @@ import "./HomePage.css";
 
 const HomePage = () => {
     return (
-        <div className="content center">
-            <div className="title grow">
-                <h1>AKSHAY SATHIYA</h1>
+        <div className="content center pt4">
+            <div className="title">
+                <h1 className="grow">AKSHAY SATHIYA</h1>
+                <div className="typewriter grow">
+                    <h3 className="bg-gray br3 ph2" style={{fontSize: "2vw"}}>Hey there, welcome to my website!</h3>
+                </div>
             </div>
         </div>
     );
--- src/components/ProjectsPage/ProjectCard.js
+++ src/components/ProjectsPage/ProjectCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import "./ProjectCard.css";
 
 const ProjectCard = ({ image, name, descriptions, link, linkType }) => {
     const cardClicked = () => {
