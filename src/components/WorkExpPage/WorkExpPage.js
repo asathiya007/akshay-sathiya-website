9846361b4ccef2d365dfb68730fea889b7c3a4e4
asathiya007 <akshay.sathiya@gmail.com>
2020-08-15 15:06:57 -0700

    updated info - August 15th, 2020

--- src/components/WorkExpPage/WorkExpPage.js
+++ src/components/WorkExpPage/WorkExpPage.js
@@ -13,7 +13,7 @@ const workExps = [
         role: "Software Engineer Intern",
         time: "May 2020 - August 2020",
         descriptions: [
-            "Developed a suggested reply system for Facebook pages representing small/medium sized businesses (SMBs) to boost messaging efficiency and increase the number of valuable business events (sales, transactions, quotes, etc.) fulfilled through the Messenger platform. This system is crucial for helping SMBs survive and operate through the COVID-19 pandemic.",
+            "Developed a suggested reply system for Facebook pages representing small/medium sized businesses (SMBs) to boost messaging efficiency and increase the number of valuable business events (sales, transactions, quotes, etc.) fulfilled through the Messenger platform. This system is crucial for helping SMBs survive and operate through the economic recession caused by the COVID-19 pandemic.",
             "Developed this system for the mobile website using Hack/PHP, JavaScript, and React.",
             "Developed this system for the Facebook Lite mobile app using Java, XML, Hack/PHP, and GraphQL."
         ],
@@ -26,8 +26,8 @@ const workExps = [
         descriptions: [
             "I developed & debugged a Google Cloud Platform (GCP) Dataflow application which flattened & mapped order data from a JSON payload to a GCP BigQuery database as a part of the company’s shift to Google Cloud Platform.",
             "I created a Confluence webpage which assisted in onboarding for the DaaS team and GCP setup.",
-            "I built an \"Age In Rack\" dashboard using React.js, Express.js, Node.js, and GCP BigQuery (full-stack web app), which displays items in a Macy’s mega-center/warehouse in tabular form, flags items that have been in storage for too long, and displays the overhead cost of storing the flagged items. Also displays a number readout and pie chart of flagged items. This project won 1st place at the 2019 Macy’s Tech Intern Hackathon for improving the supply chain. Code has been handed over to Macy’s.",
-            "I built a desktop tool and full-stack web app called \"Macy’s Acronym Dictionary\", using JDBC/Java & MySQL (desktop tool) and then React.js, Express.js, Node.js, and PostgreSQL (full-stack web app). Macy’s new hires, interns, etc. can all interact with and update a shared repository of company jargon, which immensely helps in the onboarding process and acclimation to the Macy's workplace. Code has been handed over to Macy’s."
+            "I built an \"Age In Rack\" dashboard using React, Express, Node.js, and GCP BigQuery (full-stack web app), which displays items in a Macy’s mega-center/warehouse in tabular form, flags items that have been in storage for too long, and displays the overhead cost of storing the flagged items. Also displays a number readout and pie chart of flagged items. This project won 1st place at the 2019 Macy’s Tech Intern Hackathon for improving the supply chain.",
+            "I built a desktop tool and full-stack web app called \"Macy’s Acronym Dictionary\", using JDBC/Java & MySQL (desktop tool) and then React, Express, Node.js, and PostgreSQL (full-stack web app). Macy’s new hires, interns, etc. can all interact with and update a shared repository of company jargon, which immensely helps in the onboarding process and acclimation to the Macy's workplace."
         ], 
         image: AkshayMTech
     },
