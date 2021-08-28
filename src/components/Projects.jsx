7280795e2c0344f27cbfc2887e49616d581a020d
Akshay Sathiya <akshay.sathiya@gmail.com>
2021-08-28 17:49:10 -0400

    typo fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ const Projects = () => {
         {
             image: hotspot, 
             title: 'HotSpot',
-            stack: 'Pytorch, React, Flask, and Heroku',
+            stack: 'PyTorch, React, Flask, and Heroku',
             date: 'August 2020',
             descriptions: [
                 'A machine learning tool that detects fires in aerial images captured by planes, drones, satellites, etc. Inspired by California\'s wildfire crisis in August 2020.', 
